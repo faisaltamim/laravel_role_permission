2707534dff7e47d7487b08295291284002f66956
faisaltamim <mdf41401@gmail.com>
2023-08-13 02:05:33 +0600

    template mastering

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -3,16 +3,67 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
-class RolePermissionSeeder extends Seeder
-{
+class RolePermissionSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+
+        // Create Roles
+        $roleSuperAdmin = Role::create( ['name' => 'superadmin'] );
+        $roleAdmin      = Role::create( ['name' => 'admin'] );
+        $roleEditor     = Role::create( ['name' => 'editor'] );
+        $roleUser       = Role::create( ['name' => 'user'] );
+
+        // Permission List as array
+        $permissions = [
+
+            // Dashboard
+            'dashboard.view',
+
+            // Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            // Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            // Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            // Profile Permissions
+            'profile.view',
+            'profile.edit',
+        ];
+
+        // Create and Assign Permissions
+        // for ( $i = 0; $i < count( $permissions ); $i++ ) {
+        //     // Create Permission
+        //     $permission = Permission::create( ['name' => $permissions[$i]] );
+        //     $roleSuperAdmin->givePermissionTo( $permission );
+        //     $permission->assignRole( $roleSuperAdmin );
+        // }
+        foreach ( $permissions as $permission ) {
+            // Create Permission
+            $permission = Permission::create( ['name' => $permission] );
+            $roleSuperAdmin->givePermissionTo( $permission );
+            $permission->assignRole( $roleSuperAdmin );
+        }
     }
 }
